70f743ad753e60d5861c1594181b9725c70444f4
Vikram VV <maxcache@live.com>
2016-12-27 19:49:20 +1300

    Added express server and configured gulp based on moizKachwala/Angular2-express-mongoose-gulp-node-typescript gulp settings

--- src/app/shared/firebase.config.ts
+++
@@ -1,7 +0,0 @@
-export const fireBaseConfig = {
-    apiKey: "AIzaSyC_jkS07lfUyUHzvq-eCecARXK6u16F0co",
-    authDomain: "fireblog-8360e.firebaseapp.com",
-    databaseURL: "https://fireblog-8360e.firebaseio.com",
-    storageBucket: "fireblog-8360e.appspot.com",
-    messagingSenderId: "412367499580"
-};
--- src/app/shared/shared.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
- 
-
-@Pipe({ name: 'dictionaryName' })
-export class DictionaryPipe implements PipeTransform {
-    transform(value: number, type: string): string {
-        return "";
-    }
-}
